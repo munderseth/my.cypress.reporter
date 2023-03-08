73fc9cd5fccbd03c472b7e4dafeeacd0faa7dce4
munderseth <marku@s2technologies.com>
2023-03-08 15:41:25 -0800

    limited support xml & video

---
+++ src/sandbox.js
@@ -0,0 +1,36 @@
+const xml2js = require('xml2js');
+const builder = new xml2js.Builder();
+const fs = require('fs');
+
+var results     = {};
+var testSuites  = [];
+var testCases   = [];
+var testAttachments = [];
+
+var aSuite;
+var aCase;
+var aAttachment;
+
+// Example
+var rootStats = {name: "Tests", time: "3.650", tests: 2, failures: "0"};
+var rootSuite = {name: "Root Suite", timestamp: "2023-03-01T23:04:45", file: "folder/test.js"};
+
+aSuite = rootSuite;
+testSuites.push({$: aSuite});
+
+aAttachment = "[[ATTACHMENT|dashboard.png]]";
+testAttachments.push(aAttachment);
+
+aCase = {name: "c1", classname: "class1", time: "1"};
+testCases.push({$: aCase, 'system-out': testAttachments});
+aCase = {name: "c2", classname: "class1", time: "2"};
+testCases.push({$: aCase});
+
+aSuite = {name: "s1", timestamp: "2023-03-01T23:04:45", tests: "2", time: "1" };
+testSuites.push({$: aSuite, testcase: testCases})
+
+results = { testsuites: {$: rootStats, testsuite:testSuites} }
+
+var xml = builder.buildObject(results);
+fs.writeFileSync("results.xml",xml)
+console.log(xml);
--- src/testspace-reporter.js
+++ src/testspace-reporter.js
@@ -1,25 +1,97 @@
-const Mocha = require('mocha');
+const Mocha   = require('mocha');
+const xml2js  = require('xml2js');
+const builder = new xml2js.Builder();
+const fs      = require('fs');
+const path    = require("path");
+
 const Base = Mocha.reporters.Base;
+// https://github.com/mochajs/mocha/wiki/Third-party-reporters
 const {
-  EVENT_TEST_PASS,
+  EVENT_RUN_BEGIN,
+  EVENT_RUN_END,
   EVENT_TEST_FAIL,
-  EVENT_TEST_END
+  EVENT_TEST_PASS
 } = Mocha.Runner.constants
+
+var results         = {};
+var testSuites      = [];
+var testCases       = [];
+var testAttachments = [];
+var testVideo;
+
+var aSuite;
+var aSuiteName;
+var aFileName;
+var aAttachment;
+
+const resultsDir  = 'cypress/results/';
 
 function MyReporter(runner, options) {
   Base.call(this, runner, options);
 
+  runner.on(EVENT_RUN_BEGIN, function() {
+    console.log('BEGIN ...');
+    results         = {};
+    testSuites      = [];
+    testCases       = [];
+    testAttachments = [];
+    if (!fs.existsSync(resultsDir)){
+      fs.mkdirSync(resultsDir);
+  }
+  });
+
+  runner.on(EVENT_RUN_END, function() {
+
+    // Root record
+    var rootSuite = {name: "Root Suite",  timestamp: runner.stats.start, tests: runner.stats.tests,  file: aFileName};
+    testSuites.push({$: rootSuite});
+
+    // Suite record containing testcase record(s)
+    // aAttachment = "[[ATTACHMENT|dashboard.png]]";
+    // testAttachments.push(aAttachment);
+
+    var videoFile = "cypress/videos/"+path.basename(aFileName)+".mp4";
+    testVideo = "[[ATTACHMENT|"+videoFile+"]]";
+
+    aSuite = {name: aSuiteName, timestamp: runner.stats.start, tests: runner.stats.tests, time: runner.stats.duration};
+    var aSuiteRecord = {$: aSuite, testcase: testCases, 'system-out': testVideo}
+
+    testSuites.push(aSuiteRecord)
+
+    // Stats
+    var rootStats = {name: "Mocha Tests", time: runner.stats.duration,   tests:  runner.stats.tests, failures:  runner.stats.failures};
+    results = { testsuites: {$: rootStats, testsuite:testSuites} }
+
+    var xml = builder.buildObject(results);
+    fs.writeFileSync(resultsDir+"results-"+aSuiteName+".xml",xml)
+
+    //console.log('     END: %d/%d', runner.stats.passes, runner.stats.tests);
+    console.log("END:", runner.stats)
+
+  });
+
   runner.on(EVENT_TEST_PASS, function(test) {
-    console.log('pass: %s', test.fullTitle());
+    /*
+     *  Generate the case record
+     */
+    var aCaseName  = test.title;
+    var aClassName = test.parent.title;
+    var aTime      = test.duration;
+    var aCase = {name: aCaseName, classname: aClassName, time: aTime};
+    testCases.push({$: aCase});
+
+    aSuiteName = test.parent.title;
+    aFileName  = test.parent.parent.file;
+
+    //console.log('       PASSS: %s', test.fullTitle())
+    console.log("CASE:", runner.stats)
+
   });
 
   runner.on(EVENT_TEST_FAIL, function(test, err) {
-    console.log('fail: %s -- error: %s', test.fullTitle(), err.message);
+    console.log('       FAIL: %s -- error: %s', test.fullTitle(), err.message);
   });
 
-  runner.on(EVENT_TEST_END, function() {
-    console.log('end: %d/%d', runner.stats.passes, runner.stats.tests);
-  });
 }
 
 module.exports = MyReporter;
