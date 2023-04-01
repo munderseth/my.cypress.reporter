718c819cbe4f7b869fa0bc04a81d6b0e591801f2
munderseth <marku@s2technologies.com>
2023-04-01 08:36:34 -0700

    use package only now

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,21 @@ module.exports = {
     "env": {
         "browser": true,
         "commonjs": true,
-        "es2021": true
+        "es2021": true,
+        "mocha": true
     },
     "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
     "rules": {
+    },
+    "globals": {
+        "cy": true,
+        "expect": true,
+        "Cypress": true
     }
 }
