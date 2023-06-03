f4bd1fd15909a495d3800480d5d06bc30e40b180
munderseth <marku@s2technologies.com>
2023-06-03 09:15:49 -0700

    add logging options

--- cypress.config.js
+++ cypress.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig({
         }
       };
       require('cypress-terminal-report/src/installLogsPrinter')(on, options);
-      require('cypress-xml-reporter/src/plugin') (on);
+      require('cypress-xml-reporter/src/plugin') (on, options);
       //require('cypress-mochawesome-reporter/plugin')(on); // Need to enable to use; problems using 2 plugins at the same time
     },
   },
